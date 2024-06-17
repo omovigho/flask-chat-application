7c30e782bdadbac66d8bb4086857122299d4be76
omovigho <100778865+omovigho@users.noreply.github.com>
2024-06-17 21:54:02 +0100

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,19 @@
+import os
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("tel") is None:
+            return redirect("/signup_signin")
+        return f(*args, **kwargs)
+    return decorated_function
